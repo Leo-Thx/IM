646d4d4a1b354ecda019c432a85380609dee2fbf
zengl <hans.zeng@huolala.cn>
2019-01-09 19:28:47 +0800

    remotegit

---
+++ main.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', ()=>{
+    const window = new BrowserWindow({
+        show: false
+    });
+
+    window.loadURL("http://localhost:8081");
+    // window.loadFile("dist/index.html");
+    // window.loadURL("http://www.baidu.com");
+    window.on('ready-to-show', ()=>{
+        window.show();
+    });
+
+    // BrowserWindow.addDevToolsExtension(path.resolve('./extension/shells/chrome'));
+});
