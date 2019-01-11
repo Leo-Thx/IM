a6c5ccf72f4a854a946e1a81c37f53735ddda568
zengl <hans.zeng@huolala.cn>
2019-01-11 10:41:51 +0800

    增加部分样式

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ const { app, BrowserWindow } = require('electron');
 app.on('ready', ()=>{
     const window = new BrowserWindow({
         show: false,
+		width: 350,
+		height: 500,
 		webPreferences: {
 			nodeIntegration: false,
 			preload: path.join(__dirname, 'electron/preload/main.preload.js')
 		}
     });
 
-    window.loadURL("http://localhost:8081");
-    // window.loadFile("dist/index.html");
+    // window.loadURL("http://localhost:8081");
+    window.loadFile("dist/index.html");
     // window.loadURL("http://www.baidu.com");
 
     window.on('ready-to-show', ()=>{
--- src/login/login.vue
+++ src/login/login.vue
@@ -2,10 +2,26 @@
     <div class="login-container">
         <div class="background"></div>
         <div class="content container-fluid">
-            <div class="row m-3">
-                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg">
-                <input class="form-control" type="text" placeholder="Default input">
-                <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm">
+            <div class="row avatar p-1 mt-4 mb-4 ml-auto mr-auto">
+                <img src="../../resource/user/image/avatar.jpeg"/>
+            </div>
+
+            <div class="row m-4">
+                <input class="form-control" type="text" placeholder="user-name">
+            </div>
+            <div class="row m-4">
+                <input class="form-control" type="password" placeholder="password">
+            </div>
+            <div class="row m-4">
+                <button type="button" class="btn btn-info w-100">登录</button>
+            </div>
+            <div class="row m-4 operation-btn">
+                <div class="offset-1 col-5 border-right border-info">
+                    <button type="button" class="btn btn-link btn-sm text-info">忘记密码</button>
+                </div>
+                <div class="col-5">
+                    <button type="button" class="btn btn-link btn-sm text-info">注册</button>
+                </div>
             </div>
         </div>
     </div>
@@ -22,12 +38,32 @@
 </script>
 
 <style scoped lang="scss">
+    @keyframes breathe {
+        0% {
+            box-shadow:0 1px 2px rgba(255,255,255,0.1);
+        }
+        100% {
+            box-shadow:0 1px 35px rgba(204,204,204,1);
+        }
+    }
+
     .login-container{
         height: 100%;
+        .avatar{
+            /* conic：锥形渐变 mask */
+            width: 7rem; height: 7rem; border-radius: 50%;
+
+            animation-timing-function:ease-in-out;
+            animation-name:breathe;
+            animation-duration:2700ms;
+            animation-iteration-count:infinite;
+            animation-direction:alternate;
+
+            img{ width: 100%; border-radius: 50%;}
+        }
         .background{
-            background-image: url("./../../static/image/login/background.jpeg");
+            background: url("./../../static/image/login/background.jpeg") no-repeat;
             height: 100%;
-            background-repeat: no-repeat;
             background-size: 100% 100%;
             filter: blur(2rem);
         }
@@ -35,6 +71,9 @@
             position: absolute; top: 0;
             input:focus{
 
+            }
+            .operation-btn{
+                .btn{ line-height: 1; padding: 0; }
             }
         }
     }
