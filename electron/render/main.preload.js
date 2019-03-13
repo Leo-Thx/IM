84268bf422e270c09bf18da65c0905d83ba560e7
lz-leo <leo.zenglei@gmail.com>
2019-03-13 17:50:13 +0800

    处理ipc通道声明

--- electron/render/main.preload.js
+++ electron/render/main.preload.js
@@ -1,5 +1,6 @@
 const { AngularElectron } = require("./ngElectron");
 const { remote } = require("electron");
+const { IPC_EventType } = require('./../common/IPC_EventType');
 
 // remote.require加载主进程中已经加载的模块，路径相对于主进程
 const globalVariable = remote.getGlobal('globalVariable');
@@ -7,6 +8,10 @@ const globalVariable = remote.getGlobal('globalVariable');
 Reflect.defineProperty(window, "$NgEl", {
 	value: AngularElectron
 });
+
+Reflect.defineProperty(window, '$IpcEvent', {
+    value: IPC_EventType
+})
 
 document.addEventListener('DOMContentLoaded', function(){
     // console.info(globalVariable.get(globalVariable.KEY_NAMES.REFERENCE_IM_WIN));
--- src/app/config/config.d.ts
+++ src/app/config/config.d.ts
@@ -1,4 +1,13 @@
-/// <reference types="node" />
+/**
+ * declare module|namespace 只有在内部 export后，成员才能访问
+ * 同一个命名空间的时候做文件分割的时候，三斜线引入即可
+ * 
+ * 对模块不能使用 三斜线指令, 只能用 import x from ... | import x = require
+ *      为了支持CommonJS和AMD的exports, TypeScript提供了export=语法， 但是仅对ts编译后可用
+ * 编译器首先尝试去查找相应路径下的.ts，.tsx再或者.d.ts。 如果这些文件都找不到，编译器会查找 外部模块声明
+ */
+
+/// <reference types="electron" />
 import { DesktopCapturer, IpcRenderer } from 'electron';
 
 /**
@@ -16,16 +25,20 @@ declare class NgElectron {
 /**
  * IPC 事件类型
  */
-declare namespace IPC_EventType {
-    const buildReply: (type: string)=>string;
-    const LOGIN: string;
-    const CAPTURE_SREEN: string;
+declare interface IPC_EventType {
+    readonly buildReply: (type: string)=>string;
+    readonly LOGIN: string;
+    readonly CAPTURE_SREEN: string;
 } 
 
 
+/**
+ * window对象配置
+ */
 declare global {
     interface Window {
         readonly $NgEl: NgElectron;
+        readonly $IpcEvent: IPC_EventType;
     }
 }
 
