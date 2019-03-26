7fee69d8568522f6688363c8412393d673c3e29a
lz-leo <leo.zenglei@gmail.com>
2019-03-26 19:59:48 +0800

    增加material图标

--- src/app/module/layout/sidenav/sidenav.component.ts
+++ src/app/module/layout/sidenav/sidenav.component.ts
@@ -1,5 +1,19 @@
 import { Component } from "@angular/core";
 
+export interface SideNavMenu {
+    icon: string;
+    name?: string;
+}
+
+const menus = [
+    {icon: 'message', name: '当前聊天'},
+    {icon: 'contacts', name: '联系人' },
+    {icon: 'date_range', name: '日程'},
+    {icon: 'email', name: '邮箱'},
+    {icon: 'library_music', name: '音乐'},
+    {icon: 'settings_applications', name: '设置'}
+];
+
 @Component({
     selector: 'app-sidenav',
     templateUrl: './sidenav.component.html',
@@ -8,5 +22,9 @@ import { Component } from "@angular/core";
     ]
 })
 export class SideNavComponent {
+    public menus: Array<SideNavMenu> = menus;
     
+    constructor () {
+        
+    }
 }
