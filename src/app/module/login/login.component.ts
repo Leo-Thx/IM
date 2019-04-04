461371d0d7ab4fa3c1f5b8b36c6e8cc5a0b426a6
lz-leo <leo.zenglei@gmail.com>
2019-04-04 13:58:44 +0800

    登录界面重做

--- src/app/module/login/login.component.ts
+++ src/app/module/login/login.component.ts
@@ -3,7 +3,7 @@ import { LoginService } from './login.service';
 
 import {FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormArray, FormBuilder} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
-import { state, style, trigger, transition, animate, keyframes } from '@angular/animations';
+import { state, style, trigger, transition, animate, keyframes, stagger, query } from '@angular/animations';
 import { Router } from '@angular/router';
 import { IpcService } from 'src/app/share/ipc/Ipc.service';
 import { MatSnackBar } from '@angular/material';
@@ -22,73 +22,54 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
         './login.component.scss'
     ],
     animations: [
-        trigger('openClose', [
-            // state函数来定义不同的状态，供每次转场结束时调用
-            // style定义一组与指定的状态名相关的样式。注意，样式的属性必须是小驼峰 格式的
-            // 转场[标志状态在一定时间内发生]与时序
-            state('open', style({
-                backgroundColor: 'yellow',
+        // 切换到注册 zIndex:0->2   opacity:0->1
+        // 背景切换-移动+里层透明度变化
+        // boxShadow: '0px 0px 30px 10px #e0e0e0',
+        trigger('toRegister', [
+            state('login', style({
+                zIndex: 0,
+                opacity: 0,
+                left: '60%'
             })),
-            state('closed', style({
-                backgroundColor: 'red'
+            state('register', style({
+                zIndex: 2,
+                opacity: 1,
+                left: 0
             })),
-            transition('open => closed', [
-                animate('2s ease-out', keyframes([
-                    style({ backgroundColor: 'blue' }),
-                    style({ backgroundColor: 'green' }),
-                    style({ backgroundColor: 'purple' })
-                ]))
+            transition("login=>register", [
+                animate('.5s ease-in-out'),
+                // query('.content', [
+                //     style({opacity: 0}),
+                //     stagger(0, [
+                //         animate('0.5s ease-in-out', style({
+                //             opacity: 1
+                //         })),
+                //     ])
+                // ]),
             ]),
-            transition('closed => open', [
-                animate('0.5s')
+            transition("register=>login", [
+                animate('.5s ease-in-out'),
             ])
         ]),
-        trigger('flyInOut', [
-            state('in', style({
-                transform: 'translateX(0)'
-            })),
-            transition('* => void', [
-                style({
-                    transform: 'translateX(-100%)'
-                }),
-                animate('.5s')
-            ]),
-            transition('void => *', [
-                style({
-                    transform: 'translateX(100%)'
-                }),
-                animate('1s')
-            ])
-        ]),
-        trigger('myInsertRemoveTrigger', [
-            transition(':enter', [
-                style({ opacity: 0 }),
-                animate('5s', style({ opacity: 1 })),
-            ]),
-            transition(':leave', [
-                animate('5s', style({ opacity: 0 }))
-            ])
-        ]),
+        // trigger('registerCardToRegister', [
+        //     state('login', style({
+        //         opacity: 0
+        //     })),
+        //     state('register', style({
+        //         opacity: 1
+        //     })),
+        //     transition("login=>register", [
+        //         animate('2s ease-in-out'),
+        //     ]),
+        // ])
     ]
 })
-export class LoginComponent implements AfterViewInit, OnInit, OnChanges, DoCheck{
-    title = 'loginComponent';
+export class LoginComponent {
     user: Object;
-    isOpen = true;
 
     // 单个表单控件实例
-    accountCtrl = new FormControl('71661048@qq.com');
-    // 表单组
-    groupCtrl = new FormGroup({     // 处理表单组
-        firstName: new FormControl('firstname', Validators.required),
-        lastName: new FormControl('lastname')
-    });
-    groupObject: FormGroup;
-
-    // 管理任意数量的匿名控件
-    aliases: FormArray;
-
-    matcher = new MyErrorStateMatcher();
+    public accountFc = new FormControl('71661048@qq.com');
+    public isLoginState = true;
 
     constructor(
         private loginSvc: LoginService, 
@@ -97,41 +78,25 @@ export class LoginComponent implements AfterViewInit, OnInit, OnChanges, DoCheck
         public router: Router,
         public snackBar: MatSnackBar
     ) {
-        // console.log(this.loginSvc, this.ipcSvc);
-        
         this.user = {};
-        this.useFormBuilder();
     }
 
-    onAnimationEvent (event) {
-        // console.info(event);
+    changeToReigster(){
+        this.isLoginState = false;
     }
-
-    useFormBuilder() {
-        this.groupObject = this.fb.group({
-            firstName: ['init', Validators.required],
-            lastName: [''],
-
-            aliases: this.fb.array([
-                this.fb.control('')
-            ])
-        }, {
-            validators: []
-        });
-
-        this.aliases = <FormArray>this.groupObject.get('aliases');
-        this.aliases.push(this.fb.control(''));
+    changeToLogin(){
+        this.isLoginState = true;
     }
 
     goLogin(event: MouseEvent){
         this.snackBar.open('登录成功', undefined, {
             duration: 1000
         });
-        // this.isOpen = !this.isOpen;
-        // 经过ipc到后台登录 进行socket连接
-        this.router.navigateByUrl('main');
 
-        // Notification.requestPermission(function(status) {
+        // 经过ipc到后台登录 进行socket连接
+        // this.router.navigateByUrl('main');
+
+        // Notification.requestPermission(function(status) {    // mac下有效
         //     console.info(status);
         //     if (status === "granted") {
         //         var m = new Notification('收到信息', {
@@ -144,21 +109,6 @@ export class LoginComponent implements AfterViewInit, OnInit, OnChanges, DoCheck
         //         alert('当前浏览器不支持弹出消息')
         //     }
         // });
-    }
-    
-    ngAfterViewInit() {
-        // this.accountCtrl.setValue('ceshi');
-    }
-    ngOnInit() {
-        // setTimeout(()=>{
-        //     this.demo.setValue('ceshi');
-        // }, 1000);
-    }
-    ngOnChanges(){
-        
-    }
-    ngDoCheck(){
-
     }
 }
 
--- src/app/module/public/appbar/appbar.component.ts
+++ src/app/module/public/appbar/appbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { IpcService } from 'src/app/share/ipc/Ipc.service';
 import { Observable, Subject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
 
 // 因为之前我们设置标题栏样式-webkit-app-region: drag，这里按钮必须设置样式-webkit-app-region: no-drag，不然按钮将无法选中或点击
 
@@ -15,12 +16,24 @@ export class AppTopBarComponent {
     public currentTime: Date;
     private timeSubject: Subject<Date> = new Subject();
     private timer: NodeJS.Timer;
+    public currentClasses = {};
 
-    constructor(public ipcSvc: IpcService) {
+    constructor(public ipcSvc: IpcService, public router: Router) {
         this.timeSubject.subscribe((arg)=>{
             this.currentTime = arg;
         });
         this.timer = setInterval(()=>this.timeSubject.next(new Date), 1000);
+
+        this.router.events.subscribe((event)=>{
+            if( event instanceof NavigationEnd ){
+                this.currentClasses = {};
+                if( event.urlAfterRedirects === '/login' ) {
+                    this.currentClasses = {
+                        login: true
+                    }
+                }
+            }
+       });
     }
 
     minimizable(){
