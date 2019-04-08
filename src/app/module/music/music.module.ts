2b13cc13c924d62699d35e27f4940ff17d2aa156
lz-leo <leo.zenglei@gmail.com>
2019-04-08 20:01:13 +0800

    musicbar

--- src/app/module/music/music.module.ts
+++ src/app/module/music/music.module.ts
@@ -3,6 +3,11 @@ import { ShareModule } from 'src/app/share/share.module';
 import { PublicComponentModule } from '../public/public.module';
 import { MusicMainComponent } from './music-main/music-main.component';
 
+// 主界面
+// 主界面底部播放条
+// 播放列表显示
+// 顶部状态栏播放条
+
 @NgModule({
     imports: [
         ShareModule,
