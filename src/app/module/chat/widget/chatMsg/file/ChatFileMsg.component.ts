04211c080f779bd323df9d65d51678185a8b4c3c
lz-leo <leo.zenglei@gmail.com>
2019-04-24 11:28:51 +0800

    更新ui

--- src/app/module/chat/widget/chatMsg/file/ChatFileMsg.component.ts
+++ src/app/module/chat/widget/chatMsg/file/ChatFileMsg.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from "@angular/core";
 import { MsgFileType } from 'src/app/model/msgType/FileType';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     template: `
     <span class="title block" [ngClass]="dirClass">你的名字<span class="time ml-1">12-03-12</span></span>
     <div class="flex p-mn-5 content" [ngClass]="dirClass">
-        <button class="attachment" mat-icon-button>
-            <mat-icon>insert_drive_file</mat-icon>
-        </button>
+        <mat-icon svgIcon="file_main"></mat-icon>
         <div class="detail ml-1">
             <h5 class="m-0">Tenacy Agreement.pdf</h5>
             <span>24kb Document</span>
@@ -22,5 +22,7 @@ export class ChatFileMsgComponent {
 
     @Input() public data: MsgFileType;
 
-    constructor() {}
+    constructor( iconRegistry: MatIconRegistry, sanitizer: DomSanitizer ) {
+        iconRegistry.addSvgIcon('file_main', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/chat/file_main.svg'));
+    }
 }
